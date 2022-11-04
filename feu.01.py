c670f1e5abed3711cdf2745d0743b65351d14744
nesrine <saadinesrine16@gmail.com>
2022-11-04 15:31:05 +0100

    commit

--- feu.01.py
+++ feu.01.py
@@ -10,30 +10,37 @@ def checkArguments():
 		except:
 			print('error')
 			exit()
+
 	else:
 		print("error")
 		exit()
+ 
+def checkExpression():
+	expression = sys.argv[1]
+	brackets = ['(',')']
+	operation = ['*','+','/','-','%']
+	numbers = ['0','1','2','3','4','5','6','7','8','9']
 
-def expression_between_brackets(expression):
-	index =0 
-	index_of_brackets = 0
-	expr = []
+	if expression[0] in operation or expression[-1] in operation:
+		print('error')
+		exit()
 
-	while index <len(expression):
+	for index in range(len(expression)-1):
+		if expression[index] in brackets and expression[index+1] in brackets:
+			print('error')
+			exit()
+		
+		try:
+			if expression[index] in operation and expression[index+2] not in numbers and expression[index+2] not in brackets:
+				print("error")
+				exit()
+		except:
+			exit()
 
-		if expression[index][0]=='(':
-			index_of_brackets = index
-			while expression[index][-1]!=')':
-				if len(expression[index])>1:
-					expr.append(expression[index][1:])
-				else:
-					expr.append(expression[index])
-				expression[index] = ''
-				index+=1
-			expr.append(expression[index][:-1])
-			expression[index]=''
-			return (expr,index_of_brackets)
-		index+=1
+		if (expression[index] in operation or expression[index] in numbers) and expression[index+1] in operation:
+			print("error")
+			exit()
+
 
 def priority_operation(expression):
 	result=0
@@ -41,7 +48,7 @@ def priority_operation(expression):
 	while index < (len(expression)):
 		if expression[index]== '*':
 			expression[index]=''
-			result=int(expression[index-1])*int(expression[index+1])
+			result=float(expression[index-1])*float(expression[index+1])
 			expression[index-1]=result
 			expression[index+1]=''
 			remove_blanks(expression)
@@ -50,7 +57,7 @@ def priority_operation(expression):
 
 		elif expression[index]== '/':
 			expression[index]=''
-			result=int(expression[index-1])/int(expression[index+1])
+			result=float(expression[index-1])/float(expression[index+1])
 			expression[index-1]=result
 			expression[index+1]=''
 			remove_blanks(expression)
@@ -58,7 +65,7 @@ def priority_operation(expression):
 			continue
 		elif expression[index]== '%':
 			expression[index]=''
-			result=int(expression[index-1])%int(expression[index+1])
+			result=float(expression[index-1])%float(expression[index+1])
 			expression[index-1]=result
 			expression[index+1]=''
 			remove_blanks(expression)
@@ -68,13 +75,14 @@ def priority_operation(expression):
 
 	return expression
 
+
 def non_priority_operation(expression):
 	result=0
 	index=0
 	while index < (len(expression)):
 		if expression[index]== '-':
 			expression[index]=''
-			result=int(expression[index-1])-int(expression[index+1])
+			result=float(expression[index-1])-float(expression[index+1])
 			expression[index-1]=result
 			expression[index+1]=''
 			remove_blanks(expression)
@@ -83,7 +91,7 @@ def non_priority_operation(expression):
 
 		elif expression[index]== '+':
 			expression[index]=''
-			result=int(expression[index-1])+int(expression[index+1])
+			result=float(expression[index-1])+float(expression[index+1])
 			expression[index-1]=result
 			expression[index+1]=''
 			remove_blanks(expression)
@@ -92,6 +100,7 @@ def non_priority_operation(expression):
 		index+=1
 	return expression
 
+
 def remove_blanks(expression):
 	index = 0
 	while index < len(expression):
@@ -99,29 +108,57 @@ def remove_blanks(expression):
 			expression.remove(expression[index])
 			index-=1
 		index+=1
-	return expression,index
+	return expression
 
-def nbrOfBrackets(expression):
-	nbr = 0
-	for elm in expression:
-		if elm == '(':
-			nbr +=1
-	return nbr
+def open_brackets(expression):
 
-
-expression = '3 * 4 + ((2 * 5) + (4 * 5))'
-nbrOfBrackets = nbrOfBrackets(expression)
-expression = expression.split()
+	for index in range(len(expression)-1,0,-1):
+		if expression[index][0] == '(':
+			return index
 
 
-for nbr in range(nbrOfBrackets):
-	(expression_btw_brackets,index_of_brackets)=expression_between_brackets(expression)
-	priority_operation(expression_btw_brackets)
-	non_priority_operation(expression_btw_brackets)
-	expression[index_of_brackets] = str(expression_btw_brackets[0])
-	remove_blanks(expression)
-	print(expression,expression_btw_brackets)
+def close_brackets(expression,index):
+	while index<len(expression):
+		if expression[index][-1] == ')':
+			return index
+		index+=1
+	print("error")
+	exit()
 
-priority_operation(expression)
-non_priority_operation(expression)
-print(expression[0])
+def remove_expression(expression,start,end):
+	while start<=end:
+		expression[start] = ''
+		start += 1
+
+def calculate(expression):
+	while open_brackets(expression):
+		expression_btw_brackets = []
+		last_open_bracket = open_brackets(expression)
+		last_closed_bracket = close_brackets(expression,last_open_bracket)
+
+		beginningOfExpression = expression[last_open_bracket][1:]
+		restOfExpression = expression[last_open_bracket+1:last_closed_bracket]
+		endOfExpression = expression[last_closed_bracket][:-1]
+
+		expression_btw_brackets.append(beginningOfExpression)
+		expression_btw_brackets.extend(restOfExpression)
+		expression_btw_brackets.append(endOfExpression)
+
+		priority_operation(expression_btw_brackets)
+		non_priority_operation(expression_btw_brackets)
+
+		remove_expression(expression,last_open_bracket,last_closed_bracket)
+
+		expression[last_open_bracket] = str(expression_btw_brackets[0])
+
+		remove_blanks(expression)
+
+	priority_operation(expression)
+	non_priority_operation(expression)
+	return expression
+
+
+checkArguments()
+expression = sys.argv[1].split()
+checkExpression()
+print(int(calculate(expression)[0]))
